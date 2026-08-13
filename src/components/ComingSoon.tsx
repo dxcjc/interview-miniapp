97924f3955685fabaca25774295137f4ab04d978
dxcjc <dxcjc@users.noreply.github.com>
2026-08-13 18:38:45 +0800

    style: 占位页图标换为 PNG

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from '@tarojs/components'
+import { Image, Text, View } from '@tarojs/components'
 
 /** 过渡占位页：后续阶段替换为真实功能 */
 export default function ComingSoon({ text = '功能建设中…' }: { text?: string }) {
   return (
     <View className='state-box'>
-      <View className='state-icon'>🚧</View>
+      <View className='state-icon'>
+        <Image src={require('../assets/h5/icon-target.png')} className='state-icon-img' mode='aspectFit' />
+      </View>
       <Text className='state-text'>{text}</Text>
     </View>
   )
