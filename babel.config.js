7595d20d2167f45ae6aeaf56ef2847d286d55ded
dxcjc <dxcjc@users.noreply.github.com>
2026-08-13 16:44:33 +0800

    fix: babel 转译可选链/空值合并，修复真机 invalid file common.js 报错

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 // babel-preset-taro 更多选项和默认值：
 // https://docs.taro.zone/docs/next/babel-config
+// 微信真机 JS 引擎不支持 ES2020 可选链/空值合并（报 Unexpected token .），显式转译
 module.exports = {
   presets: [
     ['taro', {
@@ -7,5 +8,9 @@ module.exports = {
       ts: true,
       compiler: 'webpack5',
     }]
-  ]
+  ],
+  plugins: [
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+  ],
 }
